074cf9beed3452e4e3805154cb28ae563241c23e
durga79 <91417489+durga79@users.noreply.github.com>
2023-05-30 20:51:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app=express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://nani:nani@cluster0.9sjwfvr.mongodb.net/todolistDB",{useNewUrlParser:true});
 const itemsSchema= {
     name:String
 };
